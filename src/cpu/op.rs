6e5a8f9097d05c924b0f5d30411011b731f9cb8e
Chen Yadong <cyd.matt@qq.com>
2023-12-31 13:08:15 +0800

    add cpu/op.rs

---
+++ src/cpu/op.rs
@@ -0,0 +1,31 @@
+pub const DEX:u8 = 0xca;
+pub const DEY:u8 = 0x88;
+pub const CLD:u8 = 0xd8;
+pub const CLI:u8 = 0x58;
+pub const CLV:u8 = 0xb8;
+pub const BRK:u8 = 0x00;
+pub const INX:u8 = 0xe8;
+pub const INY:u8 = 0xc8;
+
+pub const SEC:u8 = 0x38;
+pub const CLC:u8 = 0x18;
+
+// Jump to Subroutine
+pub const JSR:u8 = 0x20;
+
+pub const NOP:u8 = 0xea;
+pub const PHA:u8 = 0x48;
+pub const PHP:u8 = 0x08;
+pub const PLA:u8 = 0x68;
+pub const PLP:u8 = 0x28;
+pub const RTI:u8 = 0x40;
+pub const RTS:u8 = 0x60;
+pub const SED:u8 = 0xf8;
+pub const SEI:u8 = 0x78;
+
+pub const TAX:u8 = 0xaa;
+pub const TAY:u8 = 0xa8;
+pub const TSX:u8 = 0xba;
+pub const TXA:u8 = 0x8a;
+pub const TXS:u8 = 0x9a;
+pub const TYA:u8 = 0x98;
