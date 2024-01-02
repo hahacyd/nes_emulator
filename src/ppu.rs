7f1270934a5a4b82a810831ef7f68db0c89b6056
Chen Yadong <cyd.matt@qq.com>
2024-01-02 23:11:11 +0800

    add ppu

---
+++ src/ppu.rs
@@ -0,0 +1,112 @@
+use bitflags::bitflags;
+
+use crate::cartridge::Mirroring;
+
+pub struct NesPPU {
+    pub chr_rom: Vec<u8>,
+    pub palette_table: [u8; 32],
+    pub vram: [u8; 2048],
+    pub oam_data: [u8; 256],
+
+    pub mirroring: Mirroring,
+    addr: AddrRegister,
+}
+
+impl NesPPU {
+    pub fn new(chr_rom: Vec<u8>, mirroring: Mirroring) -> Self {
+        NesPPU {
+            chr_rom,
+            mirroring,
+            vram: [0; 2048],
+            oam_data: [0; 256],
+            palette_table: [0; 32],
+            addr: AddrRegister::new(),
+        }
+    }
+
+    fn write_to_ppu_addr(&mut self, value: u8) {
+        self.addr.update(value);
+    }
+}
+
+pub struct AddrRegister {
+    // hi , lo
+    value: (u8, u8),
+    hi_ptr: bool,
+}
+
+impl AddrRegister {
+    pub fn new() -> Self {
+        AddrRegister {
+            value: (0, 0),
+            hi_ptr: true,
+        }
+    }
+
+    fn set(&mut self, data: u16) {
+        self.value.0 = (data >> 8) as u8;
+        self.value.1 = (data & 0xff) as u8;
+    }
+
+    pub fn update(&mut self, data: u8) {
+        if self.hi_ptr {
+            self.value.0 = data;
+        } else {
+            self.value.1 = data;
+        }
+
+        if self.get() > 0x3fff { // mirror down addr above 0x3fff
+            self.set(self.get() & 0x3fff);
+        }
+        self.hi_ptr = !self.hi_ptr;
+    }
+
+    pub fn increment(&mut self, inc: u8) {
+        let lo = self.value.1;
+        self.value.1 = self.value.1.wrapping_add(inc);
+        if lo > self.value.1 {
+            self.value.0 = self.value.0.wrapping_add(1);
+        }
+
+        if self.get() > 0x3fff {
+            self.set(self.get() & 0x3fff);
+        }
+    }
+
+    pub fn reset_latch(&mut self) {
+        self.hi_ptr = true;
+    }
+
+    pub fn get(&self) -> u16 {
+        ((self.value.0 as u16) << 8) | (self.value.1 as u16)
+    }
+}
+
+bitflags! {
+    // 7  bit  0
+    // ---- ----
+    // VPHB SINN
+    // |||| ||||
+    // |||| ||++- Base nametable address
+    // |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+    // |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+    // |||| |     (0: add 1, going across; 1: add 32, going down)
+    // |||| +---- Sprite pattern table address for 8x8 sprites
+    // ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+    // |||+------ Background pattern table address (0: $0000; 1: $1000)
+    // ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+    // |+-------- PPU master/slave select
+    // |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+    // +--------- Generate an NMI at the start of the
+    //            vertical blanking interval (0: off; 1: on)
+    pub struct ControlRegister: u8 {
+        const NAMETABLE1              = 0b0000_0001;
+        const NAMETABLE2              = 0b0000_0010;
+        const VRAM_ADD_INCREMENT      = 0b0000_0100;
+        const SPRITE_PATTERN_ADDR     = 0b0000_1000;
+        const BACKROUND_PATTERN_ADDR  = 0b0001_0000;
+        const SPRITE_SIZE             = 0b0010_0000;
+        const MASTER_SLAVE_SELECT     = 0b0100_0000;
+        const GENERATE_NMI            = 0b1000_0000;
+    }
+}
